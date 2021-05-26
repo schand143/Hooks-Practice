9ff8729b88251ea07b1bc087d796756c1f8cbe37
schand143 <shaikhchand724@gmail.com>
2021-05-26 12:02:30 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+// import ClassCounter from './components/ClassCounter';
+// import HooksCounter from './components/HooksCounter';
+import HooksCounterTwo from './components/HooksCounterTwo';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* <ClassCounter /> */}
+      {/* <HooksCounter /> */}
+      <HooksCounterTwo />
     </div>
   );
 }
